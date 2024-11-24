2bbeb0d3141c9e366fcbf674503a4d67f74837ac
nkdey14 <im.pandey264@gmail.com>
2024-11-24 20:09:14 +0530

    Added CRUD Operation using Rest APIs

--- src/main/java/com/marketing/restapi/LeadRestController.java
+++ src/main/java/com/marketing/restapi/LeadRestController.java
@@ -3,7 +3,12 @@ package com.marketing.restapi;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -24,5 +29,42 @@ public class LeadRestController {
 		List<Lead> leads = leadRepo.findAll();
 		
 		return leads;
+	}
+	
+	//http://localhost:8084/api/leads
+	@PostMapping
+	public void saveLeadInfo(@RequestBody Lead l) {
+		
+		leadRepo.save(l);
+	}
+	
+	//http://localhost:8084/api/leads
+	@DeleteMapping("/{id}")
+	public void deleteLeadInfo(@PathVariable long id) {
+		
+		leadRepo.deleteById(id);
+	}
+	
+//	@PutMapping
+//	public void updateLeadInfo(@RequestBody Lead l) {
+//		
+//		leadRepo.save(l);
+//	}
+	
+	// Approach - 2 of updating record
+	
+	@PutMapping("/{id}")
+	public void updateLeadInfo(@PathVariable long id, @RequestBody Lead l) {
+		
+		Lead lead = leadRepo.findById(id).get();
+		
+		lead.setFirstName(l.getFirstName());
+		lead.setLastName(l.getLastName());
+		lead.setGender(l.getGender());
+		lead.setEmail(l.getEmail());
+		lead.setMobile(l.getMobile());
+		lead.setCity(l.getCity());
+		
+		leadRepo.save(lead);
 	}
 }
